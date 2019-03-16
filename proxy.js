77fd7e946fcffbbd0708467ccd806fa809990203
Gary Pang <gary.pang@gmail.com>
2019-03-16 14:18:50 -0400

    Update AWS URLs and add loader.io verification

--- proxy.js
+++ proxy.js
@@ -11,15 +11,20 @@ app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
 //serve up static files
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Verify domain ownership for Loader.io
+app.get('/loaderio-1136f36a7787cada639c4ca9b4948acd.txt', (req, res) => 
+  res.sendFile(path.resolve(__dirname, './loaderio-1136f36a7787cada639c4ca9b4948acd.txt'))
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.listen(port, err => {
   if (err) {
     return console.log(err);
   }
   console.log(`Listening on ${port}`);
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
